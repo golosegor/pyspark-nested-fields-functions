d7cdcb8b27c16431b4bf4d02e257e318a60ac682
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-05-30 17:25:32 +0200

    [init] version updated

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
-from setuptools import setup
 import subprocess
 import os
 
 remote_version = (
     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
-        .stdout.decode("utf-8")
-        .strip()
+    .stdout.decode("utf-8")
+    .strip()
 )
 
 if "-" in remote_version:
@@ -14,7 +13,7 @@ if "-" in remote_version:
     # pip has gotten strict with version numbers
     # so change it to: "1.3.3+22.git.gdf81228"
     # See: https://peps.python.org/pep-0440/#local-version-segments
-    v, i, s = remote_version.split("-")
+    v,i,s = remote_version.split("-")
     remote_version = v + "+" + i + ".git." + s
 
 assert "-" not in remote_version
@@ -24,22 +23,28 @@ assert os.path.isfile("nestedfunctions/version.py")
 with open("nestedfunctions/VERSION", "w", encoding="utf-8") as fh:
     fh.write(f"{remote_version}\n")
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 # this grabs the requirements from requirements.txt
 required_libs = {"pyspark"}
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines() if i.split("==")[0] in required_libs]
 
-setup(
-    name='pyspark_nested_fields_functions',
+
+setuptools.setup(
+    name="pyspark-nested-functions",
     version=remote_version,
     author_email="golosegor@gmail.com",
     description="Utility functions to manipulate nested structures using pyspark",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/golosegor/pyspark-nested-fields-functions",
+    packages=setuptools.find_packages(),
+    package_data={"nestedfunctions": ["VERSION"]},
+    include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
         REQUIREMENTS
     ],
-    package_data={"nestedfunctions": ["VERSION"]},
-    packages=setuptools.find_packages(where='nestedfunctions'),
-    include_package_data=True,
-    package_dir={"": "nestedfunctions"}
 )
