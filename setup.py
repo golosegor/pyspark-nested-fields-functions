f67fe426e538c3aa36519084e804a20afd91d183
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-05-30 16:04:50 +0200

    [init] version updated

--- setup.py
+++ setup.py
@@ -1,13 +1,38 @@
 from setuptools import setup, find_packages
+import subprocess
+import os
+
 
 # this grabs the requirements from requirements.txt
+
+remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in remote_version:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v,i,s = remote_version.split("-")
+    remote_version = v + "+" + i + ".git." + s
+
+assert "-" not in remote_version
+assert "." in remote_version
+
+assert os.path.isfile("nestedfunctions/version.py")
+with open("nestedfunctions/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{remote_version}\n")
+
 required_libs = {"pyspark"}
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines() if i.split("==")[0] in required_libs]
 
 
 setup(
     name='recursive_utils',
-    version='2022.5.1',
+    version=remote_version,
     include_package_data=True,
     author_email="golosegor@gmail.com",
     description="Utility functions to manipulate nested structures using pyspark",
