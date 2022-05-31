a2c146da68baec0ce550829e0e9d219882323e05
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-05-31 12:17:54 +0200

    [init] expr added

--- tests/transformations/expr/test_expr.py
+++ tests/transformations/expr/test_expr.py
@@ -36,30 +36,28 @@ class ExprTest(SparkBaseTest):
 
         self.assertEqual(["fuat@hotmail.com", "egor@hotmail.com"], parse_data(processed))
 
+    def test_expr_nested_array(self):
+        field = "emails.unverified"
 
-def test_expr_nested_array(self):
-    field = "emails.unverified"
+        def parse_data(df: DataFrame) -> List[str]:
+            return flatten([d[0] for d in df.select("emails.unverified").collect()])
 
-    def parse_data(df: DataFrame) -> List[str]:
-        return flatten([d[0] for d in df.select("emails.unverified").collect()])
+        df = parse_df_sample(self.spark,
+                             pkg_resources.resource_filename(__name__, "fixtures/nested_array_sample.json"))
+        self.assertEqual(["egorka@gmail.com\t", "juan-miguel@gmail.com\t"], parse_data(df))
 
-    df = parse_df_sample(self.spark,
-                         pkg_resources.resource_filename(__name__, "fixtures/nested_array_sample.json"))
-    self.assertEqual(["egorka@gmail.com\t", "juan-miguel@gmail.com\t"], parse_data(df))
+        processed = expr(df, field=field, expr=f"transform({field}, x -> (upper(x)))")
 
-    processed = expr(df, field=field, expr=f"transform({field}, x -> (upper(x)))")
+        self.assertEqual(["EGORKA@GMAIL.COM\t", "JUAN-MIGUEL@GMAIL.COM\t"], parse_data(processed))
 
-    self.assertEqual(["EGORKA@GMAIL.COM\t", "JUAN-MIGUEL@GMAIL.COM\t"], parse_data(processed))
+    def test_expr_on_root_level(self):
+        def parse_data(df: DataFrame) -> List[str]:
+            return [d[0] for d in df.select("email").collect()]
 
+        df = parse_df_sample(self.spark,
+                             pkg_resources.resource_filename(__name__, "fixtures/exp_sample_01.json"))
+        self.assertEqual([' Fuat@hotmail.com\t', 'test Egor@hotmail.com\n'], parse_data(df))
 
-def test_expr_on_root_level(self):
-    def parse_data(df: DataFrame) -> List[str]:
-        return [d[0] for d in df.select("email").collect()]
+        processed = expr(df, field="email", expr=f"upper(email)")
 
-    df = parse_df_sample(self.spark,
-                         pkg_resources.resource_filename(__name__, "fixtures/exp_sample_01.json"))
-    self.assertEqual([' Fuat@hotmail.com\t', 'test Egor@hotmail.com\n'], parse_data(df))
-
-    processed = expr(df, field="email", expr=f"upper(email)")
-
-    self.assertEqual([' FUAT@HOTMAIL.COM\t', 'TEST EGOR@HOTMAIL.COM\n'], parse_data(processed))
+        self.assertEqual([' FUAT@HOTMAIL.COM\t', 'TEST EGOR@HOTMAIL.COM\n'], parse_data(processed))
