47e33291a99b4de26a89b927355e7d7deb07206a
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-06-07 10:57:01 +0200

    [init] filter simplified

--- nestedfunctions/functions/filter.py
+++ nestedfunctions/functions/filter.py
@@ -1,7 +1,6 @@
+import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
-
-from nestedfunctions.processors.filter.filter_processor import FilterProcessor
 
 
 def df_filter(df: DataFrame, predicate: str) -> DataFrame:
-    return FilterProcessor(predicate).process(df)
+    return df.filter(F.expr(predicate))
