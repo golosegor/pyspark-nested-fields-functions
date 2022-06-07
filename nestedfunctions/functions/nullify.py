bbd67f1ea4e154b2f4308bd71138dbd1ff7bc301
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-06-07 10:55:57 +0200

    [init] nullify simplified

--- nestedfunctions/functions/nullify.py
+++ nestedfunctions/functions/nullify.py
@@ -1,16 +1,25 @@
+from pyspark.sql import Column, functions as F
 from pyspark.sql import DataFrame
 
-from nestedfunctions.processors.nullify.nullify_processor import NullabilityProcessor, NullabilityProcessorWithPredicate
-from nestedfunctions.processors.terminal_operation_processor_with_predicate import PredicateProcessorParameters
+from nestedfunctions.functions.terminal_operations import apply_terminal_operation, \
+    apply_terminal_operation_with_predicate
 
 
 def nullify(df: DataFrame, field: str) -> DataFrame:
-    return NullabilityProcessor(field).process(df)
+    return apply_terminal_operation(df, field,
+                                    lambda c, t: __nullify())
 
 
 def nullify_with_predicate(df: DataFrame,
                            field: str,
                            predicate_key: str,
                            predicate_value: str) -> DataFrame:
-    return NullabilityProcessorWithPredicate(field,
-                                             PredicateProcessorParameters(predicate_key, predicate_value)).process(df)
+    return apply_terminal_operation_with_predicate(df,
+                                                   field,
+                                                   lambda c, t: __nullify(),
+                                                   predicate_key,
+                                                   predicate_value)
+
+
+def __nullify() -> Column:
+    return F.lit(None)
