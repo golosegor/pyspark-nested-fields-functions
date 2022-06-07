8b399f22d2da59abf43579d1849f4ebaed94a152
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-06-07 10:49:43 +0200

    [init] binary generalization format simplified

--- nestedfunctions/functions/binary_generalization.py
+++ nestedfunctions/functions/binary_generalization.py
@@ -1,7 +1,14 @@
+import pyspark.sql.functions as F
+from pyspark.sql import Column
 from pyspark.sql import DataFrame
+from pyspark.sql.types import BooleanType
 
-from nestedfunctions.processors.binary_generalization.binary_generalization_processor import BinaryGeneralizationProcessor
+from nestedfunctions.functions.terminal_operations import apply_terminal_operation
 
 
 def binary_generalization(df: DataFrame, field: str) -> DataFrame:
-    return BinaryGeneralizationProcessor(field).process(df)
+    return apply_terminal_operation(df, field, lambda c, t: __binary_generalize(c))
+
+
+def __binary_generalize(primitive_column: Column) -> Column:
+    return (F.when(F.length(F.trim(primitive_column)) > 0, F.lit(True)).otherwise(F.lit(False))).cast(BooleanType())
--- nestedfunctions/processors/binary_generalization/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-if fields does not exist -> skip it
-
-if field is empty / null / blank with whitespaces -> 0
-otherwise it is 1
-
-"""
