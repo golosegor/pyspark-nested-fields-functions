853ffec2efdb2f0dc6a0dbd6a34759a68b781d0b
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-06-07 10:38:10 +0200

    [init] regex fixed

--- nestedfunctions/functions/str_regx_extract.py
+++ nestedfunctions/functions/str_regx_extract.py
@@ -1,10 +1,17 @@
+import pyspark.sql.functions as F
+from pyspark.sql import Column
 from pyspark.sql import DataFrame
 
-from nestedfunctions.processors.str_regexp_extract.str_regexp_extract import StringRegexpExtractProcessor
+from nestedfunctions.functions.terminal_operations import apply_terminal_operation
 from nestedfunctions.validation.validators import validate_regexp_or_throw
 
 
 def str_regx_extract(df: DataFrame, field: str, pattern: str, group_index_to_extract: int) -> DataFrame:
-    return StringRegexpExtractProcessor(column_to_process=field,
-                                        pattern=validate_regexp_or_throw(pattern),
-                                        group_index_to_extract=group_index_to_extract).process(df)
+    validate_regexp_or_throw(pattern)
+    return apply_terminal_operation(df,
+                                    field,
+                                    lambda c, t: __extract(c, pattern, group_index_to_extract))
+
+
+def __extract(primitive_value: Column, pattern: str, group_index_to_extract: int) -> Column:
+    return F.regexp_extract(primitive_value, pattern, group_index_to_extract)
