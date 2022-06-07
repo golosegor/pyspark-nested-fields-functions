ba0169566346f00f46c9513ad74f29a5ac6e2577
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-06-07 10:55:49 +0200

    [init] str concat simplified

--- nestedfunctions/functions/str_concat.py
+++ nestedfunctions/functions/str_concat.py
@@ -1,7 +1,14 @@
+import pyspark.sql.functions as F
+from pyspark.sql import Column
 from pyspark.sql import DataFrame
 
-from nestedfunctions.processors.str_concat.str_concat import StringConcatProcessor
+from nestedfunctions.functions.terminal_operations import apply_terminal_operation
 
 
 def str_concat(df: DataFrame, field: str, str_to_add: str) -> DataFrame:
-    return StringConcatProcessor(field, str_to_add).process(df)
+    return apply_terminal_operation(df, field,
+                                    lambda c, t: __str_concat(c, str_to_add))
+
+
+def __str_concat(primitive_value: Column, value_to_add: str) -> Column:
+    return F.concat(primitive_value, F.lit(value_to_add))
