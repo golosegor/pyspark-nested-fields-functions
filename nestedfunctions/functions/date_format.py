d17b0b5caa9cff7513f29ba7ef5c45839b71af42
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-06-07 10:46:46 +0200

    [init] date format simplified

--- nestedfunctions/functions/date_format.py
+++ nestedfunctions/functions/date_format.py
@@ -1,12 +1,14 @@
+import pyspark.sql.functions as F
+from pyspark.sql import Column
 from pyspark.sql import DataFrame
+from pyspark.sql.types import StringType
 
-from nestedfunctions.processors.dateformat.date_format_processor import DateFormatProcessor, \
-    DateFormatProcessorWithPredicate
-from nestedfunctions.processors.terminal_operation_processor_with_predicate import PredicateProcessorParameters
+from nestedfunctions.functions.terminal_operations import apply_terminal_operation, \
+    apply_terminal_operation_with_predicate
 
 
 def format_date(df: DataFrame, field: str, current_date_format: str, target_date_format: str) -> DataFrame:
-    return DateFormatProcessor(field, current_date_format, target_date_format).process(df)
+    return apply_terminal_operation(df, field, lambda c, t: __format_date(c, current_date_format, target_date_format))
 
 
 def format_date_with_predicate(df: DataFrame,
@@ -15,8 +17,15 @@ def format_date_with_predicate(df: DataFrame,
                                target_date_format: str,
                                predicate_key: str,
                                predicate_value: str) -> DataFrame:
-    return DateFormatProcessorWithPredicate(PredicateProcessorParameters(predicate_key=predicate_key,
-                                                                         predicate_value=predicate_value),
-                                            field,
-                                            current_date_format,
-                                            target_date_format).process(df)
+    return apply_terminal_operation_with_predicate(df,
+                                                   field,
+                                                   lambda c, t: __format_date(c,
+                                                                              current_date_format,
+                                                                              target_date_format),
+                                                   predicate_key,
+                                                   predicate_value)
+
+
+def __format_date(primitive_column: Column, current_date_format: str, target_date_format: str) -> Column:
+    date = F.to_timestamp(primitive_column.cast(StringType()), current_date_format)
+    return F.date_format(date, target_date_format).cast(StringType())
